c54bdf214e01173760936d9527418441d9ff9a4b
Megan McBride <meganambermcbride@gmail.com>
2023-04-04 16:13:47 -0400

    Set up skeleton for banner commponent

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="banner-container">
+      <div className="banner-text-container">
+        <h3> discover your wellness</h3>
+        <Link href="./product">
+          <button>shop now</button>
+        </Link>
+        {/* loop of 3+ images with ^ above */}
+      </div>
+      <img alt="essential oils" src="" className="banner-image" />
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
